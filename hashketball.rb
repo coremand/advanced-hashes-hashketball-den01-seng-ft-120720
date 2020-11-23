9f8d4628b62b3372e149fb0a5fff23e8b9b787a2
Obinna Nwabia <valinevite@gmail.com>
2020-11-23 23:08:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -153,7 +153,7 @@ end
 def player_numbers(team_name)
   output = []
   game_hash.each do |team, team_info|
-    if team_info[:team_name] == input 
+    if team_info[:team_name] == team_name 
       team_info.each do |key, value|
         if key == :players
           value.each do |player|
